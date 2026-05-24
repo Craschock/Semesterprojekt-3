7bd367efb0094221da4e2e16dd581a21e71c11ab
Craschock <msoftic@stud.hs-offenburg.de>
2026-05-24 02:18:21 +0200

    Implemented all assets and renamed them.

--- ! self made/scripts/entity components/state components/StateMachine.gd
+++ ! self made/scripts/entity components/state components/StateMachine.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 			# Save states in dictionary using node name
 			states[child.name.to_lower()] = child
 			child.transitioned.connect(on_child_transition)
-			
+	
 	if initial_state:
 		initial_state.enter()
 		current_state = initial_state
