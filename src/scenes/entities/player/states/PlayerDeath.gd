0c4fc539656fb3d4ee32d0cd2303c4d7aa40cf65
Craschock <msoftic@stud.hs-offenburg.de>
2026-06-30 04:25:56 +0200

    Fixed Death
    DIESE VERDAMMTEN ANIMATIONEN

--- src/scenes/entities/player/states/PlayerDeath.gd
+++ src/scenes/entities/player/states/PlayerDeath.gd
@@ -14,8 +14,8 @@ func enter() -> void:
 	
 	player.get_node("AnimatedSprite2D").play("death")
 	
-	# Reload the game after 1.5 seconds, change later
-	get_tree().create_timer(1.5).timeout.connect(reload_game)
+	# Reload the game after 5 seconds, change later
+	get_tree().create_timer(5).timeout.connect(reload_game)
 
 func reload_game() -> void:
 	get_tree().reload_current_scene()
