bc486187cce3a4a69c545b78b1e2cca663f07309
Craschock <msoftic@stud.hs-offenburg.de>
2026-07-24 04:49:24 +0200

    Fix for BaseEnemies so they won't run into player mindlessly

--- src/scenes/entities/Enemies/behavior_components/EnemyChase.gd
+++ src/scenes/entities/Enemies/behavior_components/EnemyChase.gd
@@ -25,17 +25,20 @@ func physics_update(_delta: float) -> void:
 	var target = PlayerManager.player
 	
 	if target != null:
-		if attack_state and enemy.global_position.distance_to(target.global_position) <= attack_range and enemy.attack_cooldown_timer <= 0.0:
-			enemy.attack_cooldown_timer = enemy.attack_cooldown
-			transitioned.emit(self, attack_state.name)
-			return
-		
-		# Which way is player?
-		var direction_vector = enemy.global_position.direction_to(target.global_position)
-		
-		# Convert vector to -1 (left), 1 (right)
-		var dir_x = sign(direction_vector.x)
+		var dist = enemy.global_position.distance_to(target.global_position)
+		var dir_x = sign(enemy.global_position.direction_to(target.global_position).x)
 		
 		if dir_x != 0:
-			enemy.velocity.x = dir_x * chase_speed
 			enemy.update_facing(dir_x)
+		
+		if dist <= attack_range:
+			if attack_state and enemy.attack_cooldown_timer <= 0.0:
+				enemy.attack_cooldown_timer = enemy.attack_cooldown
+				transitioned.emit(self, attack_state.name)
+			else:
+				enemy.velocity.x = 0.0
+				enemy.get_node("AnimatedSprite2D").play("idle")
+		else:
+			if dir_x != 0:
+				enemy.velocity.x = dir_x * chase_speed
+				enemy.get_node("AnimatedSprite2D").play(animation_name)
