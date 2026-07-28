3057cc9561e2a9e3691b3757ba1136b02633658b
Craschock <msoftic@stud.hs-offenburg.de>
2026-07-29 00:44:39 +0200

    Current state of settings. See desc
    Working mechanically. needs
    - graphics
    - sound needs to be fixed from izzy
    - and a whole lot of other new settings

--- src/scenes/ui/SettingsManager.gd
+++ src/scenes/ui/SettingsManager.gd
@@ -1,1 +1,97 @@
 extends Node
+
+# Settings saved in .cfg files. Preffered are the players settings
+const PREFERRED_PATH = "user://preferred_options.cfg"
+const DEFAULT_PATH = "user://default_options.cfg"
+
+var config = ConfigFile.new()
+
+# Values for default file
+var default_settings = {
+	"Video": {
+		"resolution_x": 1920,
+		"resolution_y": 1080,
+		"fullscreen": true,
+		"vsync": true
+	},
+	"Audio": {     # 1.0 = 100% and 0.0 = 0%
+		"master": 1.0,
+		"music": 1.0,
+		"sfx": 1.0
+	}
+	
+}
+
+func _ready() -> void:
+	load_settings()
+
+func load_settings() -> void:
+	# Place Default file if it does not exist
+	if not FileAccess.file_exists(DEFAULT_PATH):
+		_save_defaults_to_file()
+	
+	# Try to load player settings
+	var err = config.load(PREFERRED_PATH)
+	if err != OK:
+		config.load(DEFAULT_PATH)
+	
+	apply_all_settings()
+
+## Function to use when saving settings from settings UI
+func save_settings() -> void:
+	config.save(PREFERRED_PATH)
+	print("settings saved")
+
+# Creating default file
+func _save_defaults_to_file() -> void:
+	var def_config = ConfigFile.new()
+	for section in default_settings.keys():
+		for key in default_settings[section].keys():
+			def_config.set_value(section, key, default_settings[section][key])
+	
+	def_config.save(DEFAULT_PATH)
+
+
+
+# Logic for applying settings
+
+
+
+func apply_all_settings() -> void:
+	apply_video_settings()
+	apply_audio_settings()
+
+func apply_video_settings() -> void:
+	var res_x = config.get_value("Video", "resolution_x", 1920)
+	var res_y = config.get_value("Video", "resolution_y", 1080)
+	var is_fullscreen = config.get_value("Video", "fullscreen", true)
+	var vsync = config.get_value("Video", "vsync", true)
+	
+	# Set resolution
+	DisplayServer.window_set_size(Vector2i(res_x, res_y))
+	
+	# Set window mode#
+	if is_fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	# Vsync
+	if vsync:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	
+	# TODO: Add more screen settings
+
+func apply_audio_settings() -> void:
+	set_bus_volume("Master", config.get_value("Audio", "master", 1.0))
+	set_bus_volume("Music", config.get_value("Audio", "music", 1.0))
+	set_bus_volume("Sounds", config.get_value("Audio", "sfx", 1.0))
+
+func set_bus_volume(bus_name: String, linear_value: float) -> void:
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	
+	if bus_index >= 0:
+		var db_volume = linear_to_db(linear_value)
+		AudioServer.set_bus_volume_db(bus_index, db_volume)
--- src/scenes/ui/huds/SettingsUI.gd
+++ src/scenes/ui/huds/SettingsUI.gd
@@ -11,6 +11,38 @@ class_name SettingsUI
 @onready var p_sound: Control = $Settings/P_Sound
 @onready var p_controls: Control = $Settings/P_Controls
 
+
+
+# All button references
+
+# Gameplay
+
+# Video settings
+@onready var v_fullscreen_btn: CheckButton = $Settings/P_Video/fullscreen # TODO: Change to optionButton (borderless etc)
+@onready var v_res_opt: OptionButton = $Settings/P_Video/resolution 
+var available_resolutions: Array[Vector2i] = [
+	Vector2i(1280, 720), # Index 0
+	Vector2i(1366, 768), # Index 1
+	Vector2i(1600, 900), # Index 2
+	Vector2i(1920, 1080), # Index 3
+	Vector2i(2560, 1440), # Index 4
+	Vector2i(4096, 2160) # Index 5
+]
+
+# Audio
+@onready var master_slider: HSlider = $Settings/P_Sound/HSlider
+
+# Controls
+
+
+
+
+
+
+
+
+
+
 # false = Not showing settings subpanel
 # true = showing settings subpannel
 var current_state: bool
@@ -19,9 +51,102 @@ var current_state: bool
 signal settings_closed
 
 func _ready() -> void:
+	_initialize_settings()
+	_initialize_signals()
+	
 	_show_buttons()
 	_hide_Settings()
 	current_state = false
+
+func _initialize_settings() -> void:
+	var config = SettingsManager.config
+	# Gameplay
+	
+	# Video settings
+	v_fullscreen_btn.button_pressed = config.get_value("Video", "fullscreen", true)
+	v_res_opt.clear()
+	for res in available_resolutions:
+		v_res_opt.add_item(str(res.x) + " x " + str(res.y))
+	var current_res_x = config.get_value("Video", "resolution_x", 1920)
+	var current_res_y = config.get_value("Video", "resolution_y", 1080)
+	var current_res = Vector2i(current_res_x, current_res_y)
+	var index = available_resolutions.find(current_res)
+	if index != -1:
+		v_res_opt.select(index)
+	
+	# Audio
+	master_slider.value = config.get_value("Audio", "master", 1.0)
+	
+	# Controls
+	pass
+
+func _initialize_signals() -> void:
+	# Gameplay
+	
+	# Video settings
+	v_fullscreen_btn.toggled.connect(_on_fullscreen_toggled)
+	v_res_opt.item_selected.connect(_on_resolution_selected)
+	
+	# Audio
+	master_slider.value_changed.connect(_on_master_volume_changed)
+	
+	# Controls
+	
+
+
+
+
+
+
+
+
+# All settings buttons functionality
+
+# Gameplay
+
+# Video
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	SettingsManager.config.set_value("Video", "fullscreen", toggled_on)
+	SettingsManager.apply_video_settings()
+	SettingsManager.save_settings()
+
+func _on_resolution_selected(index: int) -> void:
+	var selected_res = available_resolutions[index]#
+	
+	SettingsManager.config.set_value("Video", "resolution_x", selected_res.x)
+	SettingsManager.config.set_value("Video", "resolution_y", selected_res.y)
+	
+	SettingsManager.apply_video_settings()
+	SettingsManager.save_settings()
+
+# Audio
+func _on_master_volume_changed(value: float) -> void:
+	SettingsManager.config.set_value("Audio", "master", value)
+	SettingsManager.apply_audio_settings()
+	SettingsManager.save_settings()
+
+# Controls
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Buttons
 func _show_buttons() -> void:
@@ -29,6 +154,14 @@ func _show_buttons() -> void:
 
 func _hide_buttons() -> void:
 	buttons_panel.hide()
+
+
+
+
+
+
+
+
 
 
 
@@ -70,12 +203,12 @@ func _on_b_video_pressed() -> void:
 func _on_b_sound_pressed() -> void:
 	_hide_buttons()
 	_show_Settings()
-	p_sound.hide()
+	p_sound.show()
 
 func _on_b_controls_pressed() -> void:
 	_hide_buttons()
 	_show_Settings()
-	p_controls.hide()
+	p_controls.show()
 
 func _on_b_return_to_main_pressed() -> void:
 	settings_closed.emit()
